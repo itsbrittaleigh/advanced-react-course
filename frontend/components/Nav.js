9fe411abf138dbd2b64c4f3340582ccd0faa7910
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-10-26 15:45:42 -0400

    :tada: getting react with server side rendering with next.js set up

---
+++ frontend/components/Nav.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+import NavStyles from './styles/NavStyles';
+
+const Nav = () => (
+  <NavStyles>
+    <Link href="/">
+      <a>Items</a>
+    </Link>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Sign Up</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
+);
+
+export default Nav;
