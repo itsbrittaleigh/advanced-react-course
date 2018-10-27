81fb7295875d9b6f3408c8ae075a7b137279af8b
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-10-27 14:12:59 -0400

    :sparkles: integrating graphql and prisma, creating item mutation and getting items query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  // forwarding straight from Prisma to DB
+  items: forwardTo('db'),
+  
+  // or you can use the full syntax if you need to do more than just return or send data, like check authentication
+  // async items(parent, args, context, info) {
+    // const items = await context.db.query.items();
+    // return items;
+  // }
+};
 
 module.exports = Query;
